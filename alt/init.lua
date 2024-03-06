086df5bff296ae645f6fca121eb0b28a6684b5aa
foolishprogrammer <ferry.foolish.programmer@gmail.com>
2024-03-06 07:36:22 +0700

    Personal Config > Convert to modular

---
+++ alt/init.lua
@@ -0,0 +1,13 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+require 'options'
+
+require 'keymaps'
+
+require 'lazy_bootstrap'
+
+require 'lazy_plugins'
