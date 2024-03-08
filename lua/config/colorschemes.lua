2450b1ee93af414c1fbd82b94652b130342ace45
foolishprogrammer <ferry.foolish.programmer@gmail.com>
2024-03-09 00:46:50 +0700

    [UPD] Finish the basic settings.

--- lua/config/colorschemes.lua
+++ lua/config/colorschemes.lua
@@ -1,21 +1,28 @@
 return {
-    { -- You can easily change to a different colorscheme.
-        -- Change the name of the colorscheme plugin below, and then
-        -- change the command in the config to whatever the name of that colorscheme is
-        --
-        -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-        'folke/tokyonight.nvim',
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
+    -- {
+    --     'rebelot/kanagawa.nvim',
+    --     opts = {
+    --         transparent = true,
+    --         styles = {
+    --             sidebars = 'transparent',
+    --             floats = 'transparent',
+    --         },
+    --     },
+    --     config = function()
+    --         vim.cmd 'colorscheme kanagawa'
+    --     end,
+    -- },
+    {
+        'ellisonleao/gruvbox.nvim',
+        lazy = false,
+        priority = 1000,
+        opts = {
+            transparent_mode = true,
+        },
         config = function()
-            -- Load the colorscheme here.
-            -- Like many other themes, this one has different styles, and you could load
-            -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-            vim.cmd.colorscheme 'tokyonight-night'
-
-            -- You can configure highlights by doing something like
-            vim.cmd.hi 'Comment gui=none'
+            local gruvbox = require 'gruvbox'
+            gruvbox.setup(opts)
+            vim.cmd 'colorscheme gruvbox'
         end,
     },
 }
--- vim: ts=2 sts=2 sw=2 et- vim: ts=2 sts=2 sw=2 et
--- lua/config/notify.lua
+++ lua/config/notify.lua
@@ -1,12 +1,38 @@
 return {
-    'rcarriga/nvim-notify',
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {},
     dependencies = {
-        'nvim-telescope/telescope.nvim',
+        'MunifTanjim/nui.nvim',
+        'rcarriga/nvim-notify',
+        'hrsh7th/nvim-cmp',
     },
     config = function()
-        vim.notify = require 'notify'
-        require('telescope').load_extension 'notify'
-        vim.keymap.set('n', '<leader>nh', ':Telescope notify theme=dropdown<CR>', { desc = '[N]otification [H]istory' })
+        require('noice').setup {
+            lsp = {
+                override = {
+                    ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+                    ['vim.lsp.util.stylize_markdown'] = true,
+                    ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+                },
+            },
+            presets = {
+                bottom_search = true,
+                command_palette = true,
+                long_message_to_split = true,
+                inc_rename = false,
+                lsp_doc_border = false,
+            },
+            cmdline = {
+                enabled = true,
+                view = 'cmdline',
+            },
+            vim.keymap.set(
+                'n',
+                '<leader>nh',
+                ':Telescope notify theme=dropdown<CR>',
+                { desc = '[N]otification [H]istory' }
+            ),
+        }
     end,
-    opts = {},
 }
