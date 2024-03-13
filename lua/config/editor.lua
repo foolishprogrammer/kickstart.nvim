baeba099b7da89d7317ba28097e2fcdc21bd490d
foolishprogrammer <ferry.foolish.programmer@gmail.com>
2024-03-14 05:24:58 +0700

    [UPD] add xml to emmet filetype group.

--- lua/config/editor.lua
+++ lua/config/editor.lua
@@ -30,7 +30,11 @@ return {
 
                     map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
 
-                    map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+                    map(
+                        '<leader>ws',
+                        require('telescope.builtin').lsp_dynamic_workspace_symbols,
+                        '[W]orkspace [S]ymbols'
+                    )
 
                     map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
 
@@ -52,6 +56,37 @@ return {
                             callback = vim.lsp.buf.clear_references,
                         })
                     end
+                    vim.api.nvim_create_autocmd({ 'FileType' }, {
+                        pattern = 'css,eruby,html,htmldjango,javascriptreact,less,pug,sass,scss,typescriptreact,xml',
+                        callback = function()
+                            vim.lsp.start {
+                                cmd = { 'emmet-language-server', '--stdio' },
+                                root_dir = vim.fs.dirname(vim.fs.find({ '.git' }, { upward = true })[1]),
+                                -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+                                -- **Note:** only the options listed in the table are supported.
+                                init_options = {
+                                    ---@type table<string, string>
+                                    includeLanguages = {},
+                                    --- @type string[]
+                                    excludeLanguages = {},
+                                    --- @type string[]
+                                    extensionsPath = {},
+                                    --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+                                    preferences = {},
+                                    --- @type boolean Defaults to `true`
+                                    showAbbreviationSuggestions = true,
+                                    --- @type "always" | "never" Defaults to `"always"`
+                                    showExpandedAbbreviation = 'always',
+                                    --- @type boolean Defaults to `false`
+                                    showSuggestionsAsSnippets = false,
+                                    --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+                                    syntaxProfiles = {},
+                                    --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+                                    variables = {},
+                                },
+                            }
+                        end,
+                    })
                 end,
             })
 
