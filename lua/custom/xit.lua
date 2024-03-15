de0ac2137ce6da2e8a04f3de6b5db3fe3562768f
foolishprogrammer <ferry.foolish.programmer@gmail.com>
2024-03-16 01:34:35 +0700

    [UPD] setup xit

--- lua/custom/xit.lua
+++ lua/custom/xit.lua
@@ -9,6 +9,7 @@ return {
         config = function()
             -- local augroup = vim.api.nvim_create_augroup('xit_highlights_mappings', { clear = true })
             local xit = require 'xit'
+            xit.setup()
             vim.keymap.set('n', 'ta', xit.create_new_task, { desc = '[T]ask [A]dd' })
             vim.keymap.set('n', 'tt', xit.toggle_checkbox, { desc = '[T]ask [T]oggle' })
             vim.keymap.set('n', 't]', xit.jump_to_next_task, { desc = '[T]ask Next (])' })
